4a856f47d71f55eccc7b351597be735fccb0630e
Braedan Davidson <royerbraedan@gmail.com>
2024-11-04 02:40:47 -0800

    added aria labels, added an ::after effect on the title

--- js/script.js
+++ js/script.js
@@ -2,16 +2,6 @@ const activityDiv = document.querySelector('.activity')
 const generateActivityBtn = document.querySelector('.generate-activity-btn')
 
 async function boredBotAPI() {
-    // fetch('https://apis.scrimba.com/bored/api/activity')
-    // .then(res => res.json())
-    // .then(({activity, type, participants, price})=> 
-    //     activity.innerHTML = `
-    //     <h2 class="activity-header">${activity}</h2>
-    //     <p class="activity-category">Category: ${type}</p>
-    //     <p class="activity-participants">Number of People Required: ${participants}</p>
-    //     <p class="activity-cost"> Cost: $${price}</p>
-    //     `
-    // )
     activityDiv.classList.remove('hide')
     try {
         const response = await fetch('https://apis.scrimba.com/bored/api/activity')
@@ -20,10 +10,10 @@ async function boredBotAPI() {
         }
         const {activity, type, participants, price} = await response.json()
         activityDiv.innerHTML = `
-        <h2 class="activity-header">${activity}</h2>
-        <p class="activity-category">Category: ${type}</p>
-        <p class="activity-participants">Participants: ${participants}</p>
-        <p class="activity-cost">Cost: $${price}</p>
+            <h2 class="activity-header">${activity}</h2>
+            <p class="activity-category">Category: ${type}</p>
+            <p class="activity-participants">Participants: ${participants}</p>
+            <p class="activity-cost">Cost: $${price}</p>
         `
     } catch(error) {
         console.error(error.message)
